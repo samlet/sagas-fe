cccfb7546ed1318bf615b7b229a2e9bf81e238ad
Samlet Wu <xiaofei.wu@gmail.com>
2019-12-04 18:44:39 +0800

    init version: support asr/tts

---
+++ README.md
@@ -0,0 +1,4 @@
+# sagas_fe
+
+sagas-fe中集成了tts和asr作为fe前端.
+
---
+++ lib/tasks/task_main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:sagas_fe/tasks/sytody_app.dart';
+
+void main() {
+  runApp(new SytodyApp());
+}
